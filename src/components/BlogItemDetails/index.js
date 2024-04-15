0e2738f561387582c91ba50c6d7cd13ad6dc85b4
root <you@example.com>
2024-04-15 09:58:26 +0000

    solution

--- src/components/BlogItemDetails/index.js
+++ src/components/BlogItemDetails/index.js
@@ -17,7 +17,7 @@ class BlogItemDetails extends Component {
     const {match} = this.props
     const {params} = match
     const {id} = params
-    const response = await fetch(`http://apis.ccbp.in/blogs/${id}`)
+    const response = await fetch(`https://apis.ccbp.in/blogs/${id}`)
     const data = await response.json()
     const updatedData = {
       title: data.title,
--- src/components/BlogList/index.js
+++ src/components/BlogList/index.js
@@ -9,48 +9,49 @@ import BlogItem from '../BlogItem'
 import './index.css'
 
 class BlogList extends Component {
-    state = {
-        isLoading: true, blogsData = []
-    }
+  state = {
+    isLoading: true,
+    blogsData: [],
+  }
 
-    componentDidMount() {
-        this.getBlogsData()
-    }
+  componentDidMount() {
+    this.getBlogsData()
+  }
 
-    getBlogsData = async () => {
-        const response = await fetch('http://apis.ccbp.in/blogs')
-        const data = await response.json()
-        const formattedData = data.map(eachItem => ({
-            id: eachItem.id,
-            title: eachItem.title,
-            imageUrl: eachItem.image_url,
-            avatarUrl: eachItem.avatar_url,
-            author: eachItem.author,
-            topic: eachItem.topic,
-        }))
+  getBlogsData = async () => {
+    const response = await fetch('https://apis.ccbp.in/blogs')
+    const data = await response.json()
+    const formattedData = data.map(eachItem => ({
+      id: eachItem.id,
+      title: eachItem.title,
+      imageUrl: eachItem.image_url,
+      avatarUrl: eachItem.avatar_url,
+      author: eachItem.author,
+      topic: eachItem.topic,
+    }))
 
-        this.setState({blogsData: formattedData, isLoading: false})
-    }
+    this.setState({blogsData: formattedData, isLoading: false})
+  }
 
-    render() {
-        const {blogsData, isLoading} = this.state
-        
-        return (
-            <div className="blogs-list-container">
-                {isLoading ? (
-                    <div data-testid="loader">
-                        <Loader type="TailSpin" color="#00bfff" height={50} width={50} />
-                    </div>
-                ) : (
-                    <ul className="blogs-list">
-                        {blogsData.map(eachBlogItem => (
-                            <BlogItem key={eachBlogItem.id} blogItemDetails={eachBlogItem} />
-                        ))}
-                    </ul>
-                )}
-            </div>
-        )
-    }
+  render() {
+    const {blogsData, isLoading} = this.state
+
+    return (
+      <div className="blogs-list-container">
+        {isLoading ? (
+          <div data-testid="loader">
+            <Loader type="TailSpin" color="#00bfff" height={50} width={50} />
+          </div>
+        ) : (
+          <ul className="blogs-list">
+            {blogsData.map(eachBlogItem => (
+              <BlogItem key={eachBlogItem.id} blogItemDetails={eachBlogItem} />
+            ))}
+          </ul>
+        )}
+      </div>
+    )
+  }
 }
 
 export default BlogList
